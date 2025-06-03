75e128e3b4f8ef53031201476515458c368167ee
fedora-g14 <jahumphreys2060@gmail.com>
2025-06-03 13:49:30 -0500

    altered logging setup and build for testing

--- src/main.zig
+++ src/main.zig
@@ -4,17 +4,19 @@ const App = @import("App.zig");
 
 const logging = @import("error_logging.zig");
 pub const std_options: std.Options = .{
-    .logFn = logging.myLogFn, // Use your custom log function
+    .logFn = logging.log,
 };
 
 pub fn main() !void {
     try logging.init();
     var app = try App.init(std.heap.page_allocator);
-    defer app.deinit() catch {};
     while (true) {
         app.run() catch |err| switch (err) {
             App.Signal.Exit => break,
             else => return err,
         };
     }
+    defer app.deinit() catch |err|{
+        std.log.err("{any}", .{err});
+    };
 }
--- src/root.zig
+++
@@ -1,4 +0,0 @@
-const logging = @import("error_logging.zig");
-pub const std_options = struct {
-    pub const logFn = logging.myLogFn; // Use your custom log function
-};
