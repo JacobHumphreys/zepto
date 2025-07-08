1fd506f2bec84c619aa9febb511998520e2a6e5b
Jacob Humphreys - System Admin <jahumphreys2060@gmail.com>
2025-07-07 20:00:15 -0500

    removed extranious @as() from cast

--- src/lib/casts.zig
+++ src/lib/casts.zig
@@ -1,4 +1,4 @@
 /// An inline function alias for @as(T, @intCast(value))
 pub inline fn intCast(comptime T: type, value: anytype) T {
-    return @as(T, @intCast(value));
+    return @intCast(value);
 }
