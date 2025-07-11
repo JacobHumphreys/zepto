a955a2e8f7064b79c21ffd44f1f74ca58d3dd787
Jacob Humphreys - System Admin <jahumphreys2060@gmail.com>
2025-07-11 02:11:07 -0500

    mvoed RenderElement to lib

---
+++ src/lib/types/RenderElement.zig
@@ -0,0 +1,12 @@
+//! Used to represent a UI element with a toString method, position, and visibility state.
+//! Additionally a cursorContainer can be attatched if the Stringable struct also handles
+//! cursor state.
+const interfaces = @import("../interfaces.zig");
+const Stringable = interfaces.Stringable;
+const CursorContainer = interfaces.CursorContainer;
+const Vec2 = @import("Vec2.zig");
+
+stringable: Stringable,
+cursorContainer: ?CursorContainer = null,
+position: Vec2,
+is_visible: bool,
