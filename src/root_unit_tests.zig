2595035a6f3b151e8b1f28993746c80839549161
JacobHumphreys <jahumphreys2060@gmail.com>
2025-09-15 21:33:45 -0600

    added call to file unit tests

--- src/root_unit_tests.zig
+++ src/root_unit_tests.zig
@@ -2,10 +2,12 @@ const renderables = @import("App/ui/renderables.zig");
 const Ribbon = renderables.Ribbon;
 const TextWindow = renderables.TextWindow;
 const input = @import("App/input.zig");
+const files = @import("App/files.zig");
 
 //This is here because zig would not test my files the correct way when using normal building
 test "Run All Tests" {
     _ = TextWindow;
     _ = Ribbon;
     _ = input;
+    _ = files;
 }
